da7dd17dca96289720ec90ed85f953bd86f40c8d
unenlightened <adept_inept@yahoo.com>
2019-02-20 23:33:01 -0800

    block page structure and Nav and Footer components

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import Nav from './Nav'
+import Footer from './Footer'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <header>Newsboard</header>
         <h1>Hello World</h1>
+        <Nav />
+        <main>
+          page content
+        </main>
+        <Footer />
       </div>
     );
   }
